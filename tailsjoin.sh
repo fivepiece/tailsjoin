7e60bbaee9c207bd6dc661551c8983c649ddd41f
fivepiece <fivepiece@users.noreply.github.com>
2016-09-17 16:06:24 +0300

    cosmetics

--- tailsjoin.sh
+++ tailsjoin.sh
@@ -36,7 +36,7 @@ init_msgs()
     msgs[script_goal_minimal]="\nThis script will install Joinmarket and its dependencies on a minimal tails OS without a local blockchain.\nYou will be using the blockr.io api for blockchain lookups (always over tor).\n\n"
     msgs[script_goal_full]="\nThis script will install Joinmarket and its dependencies on a minimal tails OS with a local blockchain.\nThis requires both persistence and at least 90GB of free space\n\n"
     msgs[script_goal_fail]="\nFull node setup not supported without persistence.\nCreate a persistent volume with at least 90GB of free space and restart tailsjoin.sh"
-    msgs[persist_on_wrong_dir]="\nIT SEEMS YOU HAVE PERSISTENCE ENABLED, BUT YOU ARE IN THE FOLDER:\n\n${PWD}\n\nIF YOU MOVE THE tailsjoin/ FOLDER TO /home/amnesia/Persistent/\nYOUR INSTALL WILL SURVIVE REBOOTS, OTHERWISE IT WILL NOT.\n\nQUIT THE SCRIPT NOW TO MOVE? (y/n) "
+    msgs[persist_on_wrong_dir]="\nIT SEEMS YOU HAVE PERSISTENCE ENABLED, BUT YOU ARE IN THE FOLDER:\n\n${PWD}\n\nIF YOU MOVE THE tailsjoin/ FOLDER TO /home/amnesia/Persistent/\nYOUR INSTALL WILL SURVIVE REBOOTS, OTHERWISE IT WILL NOT.\n\n"
     msgs[persist_mode_on]="\nPersistence is enabled and working directory is in persistent volume.\n"
     msgs[persist_mode_off]="\nPERSISTENCE IS DISABLED!\nABSOLUTELY NO DATA WILL SURVIVE A REBOOT.\n\n"
     msgs[warn_apt_install]="\nInstalling dependencies:\n\napt : ${apt_deps_jessie} ${apt_deps_testing}\npip : ${pip_deps}\n\nYou will be asked to input a password for sudo."
@@ -98,7 +98,7 @@ check_persitence()
         else
 
             echo -e "${msgs[persist_mode_on]}"
-            read -p "Continue installing Joinmarket in persistence mode? (y/n)" q
+            read -p "Continue installing Joinmarket in persistence mode? (y/n) " q
 
             if [[ "${q}" =~ [Yy] ]]; then
 
@@ -301,9 +301,10 @@ main()
         make_core_cfg
     fi
     clear
+
+    echo -e "\nJoinmarket installed in: ${jm_home}"
+    cd "${jm_home}"
 }
 
 main "${1}"
-echo -e "\nJoinmarket installed in: ${jm_home}"
-cd "${jm_home}"
 exit 0
